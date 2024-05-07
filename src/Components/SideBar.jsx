1e1875afcb93e1ae487a2b2cc0603cee7a2ec8c5
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-05-07 21:02:00 +0300

    phase2-project

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components'
+import styled from 'styled-components';
 import { useGlobalContext } from '../context/global';
 
-
 function Sidebar() {
-    const {popularAnime} = useGlobalContext()
+    const { popularAnime } = useGlobalContext();
 
-    const sorted = popularAnime?.sort((a,b) => {
-        return b.score - a.score
-    })
+    const sorted = popularAnime?.sort((a, b) => {
+        return b.score - a.score;
+    });
 
     return (
         <SidebarStyled>
             <h3>Top 5 Popular</h3>
             <div className="anime">
-                {sorted?.slice(0,5).map((anime) => {
-                    return <Link to={/anime/${anime.mal_id}} key={anime.mal_id}>
+                {sorted?.slice(0, 5).map((anime) => (
+                    <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
                         <img src={anime.images.jpg.large_image_url} alt="" />
-                        <h5>
-                            {anime.title}
-                        </h5>
+                        <h5>{anime.title}</h5>
                     </Link>
-                })}
+                ))}
             </div>
         </SidebarStyled>
-    )
+    );
 }
 
 const SidebarStyled = styled.div`
@@ -35,26 +32,30 @@ const SidebarStyled = styled.div`
     padding-right: 5rem;
     padding-left: 2rem;
     padding-top: 2rem;
-    .anime{
+
+    .anime {
         display: flex;
         flex-direction: column;
         width: 150px;
-        img{
+
+        img {
             width: 100%;
             border-radius: 5px;
             border: 5px solid #e5e7eb;
         }
-        a{
+
+        a {
             margin-top: 1rem;
             display: flex;
             flex-direction: column;
-            gap: .4rem;
-            color: #27AE60;
-            h4{
+            gap: 0.4rem;
+            color: #27ae60;
+
+            h5 {
                 font-size: 1.1rem;
             }
         }
     }
 `;
 
-export default Sidebar
+export default Sidebar;
